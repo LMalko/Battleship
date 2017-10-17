fed337ddfd1ddb1baa81beeab2715f40c5baef5e
LMalko <lmalko2601@gmail.com>
2017-10-17 11:08:53 +0200

    Spisane z kartki wersja alpha

--- player.py
+++ player.py
@@ -4,5 +4,20 @@ from ship import *
 from square import *
 
 
-class Player():
-    pass
+class Player(GameFlow):
+    
+    ships_plan: {}
+    board = None
+    name = "Noname"
+
+    def __init__(self, ship_instance):
+
+        self.name = ship_instance
+
+    def perform_hit(self, opponent_instance, coordinates):
+
+        self.board = coordinates
+
+    def choose_ships_placement(self, this_players_plan):
+        
+        self.ships_plan = this_players_plan
--- square.py
+++ square.py
@@ -10,12 +10,15 @@ class Square():
     associated_class = None
 
     def __init__(self, ship_instance):
+
         self.associated_class = ship_instance
 
     def was_hit(self):
+
         self.hit_count = 1
         self.handle_hit()
 
     def handle_hit(self):
+        
         if isinstance("Ship"):
             self.associated_class.decrement_hp()
