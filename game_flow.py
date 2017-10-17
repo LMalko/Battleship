273f608be8ba759e1ddaa7514d56f5d7f9181fb5
Leanka <comamydzisiajnasniadanie@gmail.com>
2017-10-18 00:29:15 +0200

    add fight and chcecking if lose methods

--- game_flow.py
+++ game_flow.py
@@ -15,7 +15,32 @@ class GameFlow():
         self.player_one = self.choose_play_mode()
         self.player_two = Human(self.choose_players_name())
 
-    # @staticmethod
+    def fight(self):
+        # wywołane z maina, pętla  zt zwycięzcę(Playera) (?)
+        # z maina można go wpisać do hall of fame itp
+        # zaczyna wybrany czy losowy gracz?
+        while True:
+            self.turn_count += 1
+            self.player_one.perform_hit(self.player_two): # brak drugiego arg - coordinants powinien pobierać już w perform_hit()
+            if self.check_if_lose(self.player_two):
+                return self.player_one
+            self.player_two.perform_hit(self.player_one)    # -||-
+            if self.check_if_lose(self.player_one):
+                return self.player_two
+
+
+    def check_if_lose(self, player):    #obj klasy Player (któryś z graczy)
+        # spr sumę punktów życia posiadanych statków. Jeśli == 0 to przegrał
+        # w Ocean można zrobić dodatkową liste przechowującą referencję do każdego statku umieszczonego na boardzie
+        # w ten sposób łatwiej będzie tu sumować W PĘTLI ich punkty życia:
+        #  total_hit_points = 0
+        #  for ship in player.board.MOJA_FLOTA:
+        #      total_hit_points += ship.hit_points
+        #  if total_hit_points == 0:
+        #       return True
+        #  return False
+        
+
     def choose_play_mode(self):
         play_modes = ['Choose game mode:', ' 1. Singleplayer', ' 2. Multiplayer']
         while True:
