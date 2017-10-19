acaedefb9d8df728f5bdbb6a91e5e1a3525f23a9
jarqprog <jarqprog@gmail.com>
2017-10-19 16:24:25 +0200

    refactor abrain (private meths)

--- abrain.py
+++ abrain.py
@@ -23,25 +23,25 @@ class ABrain():
     def search_and_try_destroy(self, opponent):
         """Start searching & shooting process, returns coordinates."""
         # if the last accurate shot was not long ago:
-        if self.check_if_bother_last_accurate_coords(opponent):
-            # try in fields in neighborhood:
-            coords = self.check_coords_next_to(opponent)
+        if self.__check_if_bother_last_accurate_coords(opponent):
+            # try in ocean_fields in neighborhood:
+            coords = self.__check_coords_next_to(opponent)
         else:
-            # or try in new fields
-            coords = self.check_new_coords(opponent)
+            # or try in new ocean_fields
+            coords = self.__check_new_coords(opponent)
         return coords
 
-    def was_player_hit(self, x_coord, y_coord, opponent):
+    def __was_player_hit(self, x_coord, y_coord, opponent):
         """Check if opponent's ship was hit, returns bool."""
-        condition_1 = opponent.board.fields[x_coord][y_coord].single_square_hit_count == 0
-        condition_2 = opponent.board.fields[x_coord][y_coord].associated_class
+        condition_1 = opponent.board.ocean_fields[x_coord][y_coord].single_square_hit_count == 0
+        condition_2 = opponent.board.ocean_fields[x_coord][y_coord].associated_class
         if condition_1 and condition_2:
             self.last_accurate_coords = (x_coord, y_coord)
             return True
         else:
             return False
 
-    def check_new_coords(self, opponent):
+    def __check_new_coords(self, opponent):
         """
         Check (random choice) new attack coordinates.
         tries_number: (integer) specify number of tries, formula:
@@ -49,60 +49,60 @@ class ABrain():
         tries_modifier: (integer) higher means more tries for AI.
         Returns attack coordinates.
         """
-        tries_modifier = 1
+        tries_modifier = 2
         tries_number = self.intelligence * tries_modifier
         coords = (10, 10)  # starting tmp coords
         for tries in range(tries_number):
             for check in range(40):
                 x_coord = random.randint(0, 9)
                 y_coord = random.randint(0, 9)
-                if self.check_if_new_coords_in_board_and_not_in_memo(
+                if self.__check_if_new_coords_in_board_and_not_in_memo(
                                                                     x_coord,
                                                                     y_coord):
 
                     break
-            checker = self.was_player_hit(x_coord, y_coord, opponent)
+            checker = self.__was_player_hit(x_coord, y_coord, opponent)
             coords = (x_coord, y_coord)
-            self.remember_used_coords(coords, checker)
+            self.__remember_used_coords(coords, checker)
             if checker:
                 return coords
         # if can't find any unused field to attack:
         if coords == (10, 10):
-            coords = self.find_field_in_desperado_mode(opponent)
+            coords = self.__find_field_in_desperado_mode(opponent)
             return coords
         else:
             return coords
 
-    def check_coords_next_to(self, opponent):
-        """Check fields in neighborhood of last accurate shot."""
+    def __check_coords_next_to(self, opponent):
+        """Check ocean_fields in neighborhood of last accurate shot."""
         x_coord = self.last_accurate_coords[0]
         y_coord = self.last_accurate_coords[1]
         if self.should_search_vertical or self.should_search_horisontal:
             if self.should_search_vertical and self.intelligence != 1:
-                coords = self.search_horison_or_vert(
+                coords = self.__search_horison_or_vert(
                                                     x_coord, y_coord,
                                                     opponent,
                                                     mode="vertical")
             else:
-                coords = self.search_horison_or_vert(
+                coords = self.__search_horison_or_vert(
                                                     x_coord, y_coord,
                                                     opponent,
                                                     mode="horisontal")
         else:
             decide_if_horisontal = random.choice((True, False))
             if decide_if_horisontal:
-                coords = self.search_horison_or_vert(
+                coords = self.__search_horison_or_vert(
                                                     x_coord, y_coord,
                                                     opponent,
                                                     mode="horisontal")
             else:
-                coords = self.search_horison_or_vert(
+                coords = self.__search_horison_or_vert(
                                                     x_coord, y_coord,
                                                     opponent,
                                                     mode="vertical")
         return coords
 
-    def check_if_bother_last_accurate_coords(self, opponent):
+    def __check_if_bother_last_accurate_coords(self, opponent):
         """
         Check if it makes sense to relate to last accurate shot.
         acceptable_topicality: (integer) increases the tolerance
@@ -122,12 +122,12 @@ class ABrain():
                 return True
         return False
 
-    def remember_used_coords(self, coords, checker):
+    def __remember_used_coords(self, coords, checker):
         """Save result of last shot in memory."""
         if coords not in self.ai_memo:
             self.ai_memo[(coords)] = checker
 
-    def check_if_new_coords_in_board_and_not_in_memo(self, x_coord, y_coord):
+    def __check_if_new_coords_in_board_and_not_in_memo(self, x_coord, y_coord):
         """
         Take coordinates and check if they're not in memory (so they're fresh).
         Check if coordinates are in correct range (0, 9).
@@ -141,13 +141,13 @@ class ABrain():
             return True
         return False
 
-    def search_horison_or_vert(
+    def __search_horison_or_vert(
                                 self, x_coord, y_coord,
                                 opponent, mode="horisontal"):
         """
         Start searching in horisontal or vertical mode (modify x or y coord)
         for better accuracy.
-        Used while searching fields near last accurate shoot.
+        Used while searching ocean_fields near last accurate shoot.
         """
         if mode == "horisontal":
             coord = y_coord
@@ -159,14 +159,14 @@ class ABrain():
                 y_coord = coord
             else:
                 x_coord = coord
-            if self.check_if_new_coords_in_board_and_not_in_memo(
+            if self.__check_if_new_coords_in_board_and_not_in_memo(
                                                             x_coord,
                                                             y_coord):
-                checker = self.was_player_hit(
+                checker = self.__was_player_hit(
                                                 x_coord,
                                                 y_coord, opponent)
                 coords = (x_coord, y_coord)
-                self.remember_used_coords(coords, checker)
+                self.__remember_used_coords(coords, checker)
                 if checker:
                     if mode == "horisontal":
                         self.should_search_horisontal = True
@@ -178,22 +178,22 @@ class ABrain():
                 self.should_search_horisontal = False
             else:
                 self.should_search_vertical = False
-            coords = self.check_new_coords(opponent)
+            coords = self.__check_new_coords(opponent)
             return coords
 
-    def find_field_in_desperado_mode(self, opponent):
+    def __find_field_in_desperado_mode(self, opponent):
         """Search for any field to attack. Iterate over all board."""
         x_coord = 0
         y_coord = 0
         coords = (x_coord, y_coord)
         for row in range(10):
             for column in range(10):
-                if self.check_if_new_coords_in_board_and_not_in_memo(
+                if self.__check_if_new_coords_in_board_and_not_in_memo(
                                                                     x_coord,
                                                                     y_coord):
-                    checker = self.was_player_hit(x_coord, y_coord, opponent)
+                    checker = self.__was_player_hit(x_coord, y_coord, opponent)
                     coords = (x_coord, y_coord)
-                    self.remember_used_coords(coords, checker)
+                    self.__remember_used_coords(coords, checker)
                     if checker:
                         return coords
                 y_coord += 1
