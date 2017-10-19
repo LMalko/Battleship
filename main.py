2c7c88f04a28d841b4f0051b5796825248ee4d37
LMalko <lmalko2601@gmail.com>
2017-10-19 18:00:55 +0200

    added getch

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 import time
 from game_flow import *
+from ship_position_picker import getch_single_character
 
 
 def delay_print(s):
@@ -35,9 +36,10 @@ def main():
     print("The winner is: ", winner.name)
     print("Zostałeś żeglarzem roku!")
     elapsed_time = str((time.time() - start_time) / 60).split(".")[0]
-    print("Your result,", winner.name, ", is:", this_game.turn_count, "in", elapsed_time, "minutes.")
+    print("Your result,", winner.name, "is:", this_game.turn_count, "in", elapsed_time, "minutes.")
     this_game.init_hall_of_fame(str(this_game.turn_count), winner.name, elapsed_time)
-    time.sleep(6)
+    print("Press enter to continue.")
+    getch_single_character()
     this_game.show_hall_of_fame()
 
 
